43ffeb9c9d545f793adac92285fd844461a6d8ca
Benjamin Sepulveda <benjaminsepulveda.0420@gmail.com>
2024-03-21 14:09:22 -0300

    boton

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,7 @@ function solicitarMonto() {
     let monto = prompt("Ingrese la cantidad que desea transferir:");
     
     if (monto !== null && monto !== "") {
-        let confirmacion = confirm("¿Está seguro que desea transferir $" + monto + "?");
+        let confirmacion = confirm(`¿Está seguro que desea transferir ${monto}?`);
 
         if (confirmacion) {
             alert("Transferencia realizada exitosamente.");
